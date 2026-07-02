631314c66a02cc7c9b978466c5094c72afcbe05e
Adrian Kunz <a.kunz@uni-kassel.de>
2026-07-02 15:02:35 +0200

    fix: Wrong date format in DE

--- projects/lib/src/lib/behaviors/localization/locales/de.ts
+++ projects/lib/src/lib/behaviors/localization/locales/de.ts
@@ -24,10 +24,10 @@ const de:IPartialLocaleValues = {
         ],
         formats: {
             time: "HH:mm",
-            datetime: "d. MMMM YYYY HH:mm",
-            date: "d. MMMM YYYY",
-            month: "MMMM YYYY",
-            year: "YYYY"
+            datetime: "d. MMMM yyyy HH:mm",
+            date: "d. MMMM yyyy",
+            month: "MMMM yyyy",
+            year: "yyyy"
         },
         firstDayOfWeek: 1
     },
